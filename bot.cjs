bd3338203565e00f083fa27fabd0aaa902990f25
In Kun Chun <86516664+inkunchun@users.noreply.github.com>
2024-03-28 12:08:15 -0400

    Add files via upload

---
+++ bot.cjs
@@ -0,0 +1,39 @@
+require("dotenv").config()
+
+const m = require('masto')
+
+const masto = m.createRestAPIClient({
+    url: "https://networked-media.itp.io/",
+    accessToken: process.env.TOKEN
+})
+
+async function makeStatus(text){
+    
+    let rand = Math.floor(Math.random() * 224) +1
+    const url = "https://api.adviceslip.com/advice/" + rand;
+    let response = await fetch(url);
+
+    let jsonData = await response.json();
+    success(jsonData);
+}
+
+async function success(response){
+    const status = await masto.v1.statuses.create({
+        status: response.slip.advice,
+        visibility: "public"
+    })
+
+    console.log(status.url)
+}
+
+// makeStatus()
+setInterval(()=>{
+    // let emo = [":)",":(",":'(",":P"]
+    // let message = emo[rand]
+    makeStatus()
+}, 5000)
+//604800000
+
+function err(error) {
+    console.log(error);
+}
